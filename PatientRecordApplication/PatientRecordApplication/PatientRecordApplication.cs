3cff2f0f7cf84e1504b3e5e819072fea62d63f75
Ali Seklawi <aseklawi@umich.edu>
2022-02-15 12:37:24 -0500

    Update1

--- PatientRecordApplication/PatientRecordApplication/PatientRecordApplication.cs
+++ PatientRecordApplication/PatientRecordApplication/PatientRecordApplication.cs
@@ -1,1 +1,25 @@
+using System;
+using System.IO;
 
+namespace ConsoleApp
+{
+    class Patient
+    {
+        private int id;
+        private string name;
+        private double balance;
+        public Patient(int id, string name, double balance)
+        {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+        public void wirteToFile()
+        {
+            using (StreamWriter w = File.AppendText("Patients.txt"))
+            {
+                w.WriteLine("ID : " + this.id + ", Name : " + this.name + ", Balance " + this.balance, w);
+            }
+        }
+    }
+}
--- PatientRecordApplication/PatientRecordApplication/Program.cs
+++ PatientRecordApplication/PatientRecordApplication/Program.cs
@@ -10,6 +10,31 @@ namespace PatientRecordApplication
     {
         static void Main(string[] args)
         {
+            int id;
+            string name;
+            double balance;
+
+
+            Console.Write("Patient ID : ");
+            id = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Patient Name : ");
+            name = Console.ReadLine();
+
+
+            Console.Write("Patient Balance : ");
+            balance = double.Parse(Console.ReadLine());
+
+
+            Patient p = new Patient(id, name, balance);
+            p.wirteToFile();
+
+
+            Console.WriteLine("Record has been added to file.");
+            Console.ReadKey();
         }
     }
 }
+
+
